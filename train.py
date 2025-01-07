c4dc5ad36685ebb1887555ec8e0de4708be8e65d
Vamsi Uppuluri <uvamsi76@gmail.com>
2025-01-07 10:56:23 +0530

    added gradient clipping and beta eps in adamw. And also back to personal laptop GPU :/ but its fine as these are param changes not gpu optimizations

--- train.py
+++ train.py
@@ -33,11 +33,11 @@ model.eval()
 model.to(device)
 model=torch.compile(model)
 
-train_loader=DataLoaderLite(B=16, T=1024)
-
+# train_loader=DataLoaderLite(B=16, T=1024)
+train_loader=DataLoaderLite(B=2, T=512)
 # logits,loss=model(x,y)
 # print(loss)
-optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4)
+optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4, betas=(0.9,0.95),eps=1e-8)
 
 for i in range(50):
     t0=time.time()
@@ -49,6 +49,9 @@ for i in range(50):
     with torch.autocast(device_type=device,dtype=torch.bfloat16):
         logits,loss=model(x,y)
     loss.backward()
+
+    norm=torch.nn.utils.clip_grad_norm_(model.parameters() , 1.0 )
+
     optimizer.step()
     
     torch.cuda.synchronize()
@@ -56,7 +59,7 @@ for i in range(50):
     t1=time.time()
     dt = (t1-t0) * 1000
     tokens_per_sec=(train_loader.B*train_loader.B)/(t1-t0)
-    print(f"step: {i} ------> loss: {loss.item()}------------> dt: {dt:.2f}ms--------> tokens/sec:{tokens_per_sec:.2f}")
+    print(f"step: {i} ------> loss: {loss.item()}----------->Norm: {norm:.4f} ------------> dt: {dt:.2f}ms--------> tokens/sec:{tokens_per_sec:.2f}")
 
 
 
